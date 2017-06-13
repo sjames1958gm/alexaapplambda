08fe613b39b951dd494ce9d83229a6c1a667d497
sjames1958gm <sajames1958@gmail.com>
2017-06-13 13:19:56 -0500

    add latency, video app, move presenations to generic nzos

--- lambda.js
+++ lambda.js
@@ -13,6 +13,9 @@ var {presentations_handlers, presentations_app_id, presentations_strings} = requ
 var {nzos_handlers, nzos_app_id, nzos_strings} = require("./nzos");
 var {telegram_handlers, telegram_app_id, telegram_strings} = require("./telegram");
 
+var latencyAppId = "amzn1.ask.skill.43994ac0-16d2-4804-8dd1-2beba59fba43";
+var videoAppId = "amzn1.ask.skill.c8e79877-8278-4628-8076-4b7ed6b8bfef";
+
 exports.handler = function (event, context) {
     console.log(JSON.stringify(event));
 
@@ -22,11 +25,18 @@ exports.handler = function (event, context) {
     let app = "";
     
     switch (event.session.application.applicationId) {
+        // case presentations_app_id:
+        //     app = "presentations";
+        //     alexa.appId = presentations_app_id;
+        //     alexa.resources = presentations_strings;
+        //     alexa.registerHandlers(presentations_handlers);
+        // break;
         case presentations_app_id:
             app = "presentations";
             alexa.appId = presentations_app_id;
-            alexa.resources = presentations_strings;
-            alexa.registerHandlers(presentations_handlers);
+            alexa.resources = nzos_strings;
+            alexa.registerHandlers(nzos_handlers);
+            context.appName = "presentations";
         break;
         case nzos_app_id:
             app = "cloud OS";
@@ -39,6 +49,21 @@ exports.handler = function (event, context) {
             alexa.appId = telegram_app_id;
             alexa.resources = telegram_strings;
             alexa.registerHandlers(telegram_handlers);
+        break;
+        case latencyAppId:
+            app = "Test App";
+            alexa.appId = latencyAppId;
+            alexa.resources = nzos_strings;
+            alexa.registerHandlers(nzos_handlers);
+            context.appName = "latency";
+        break;
+        case videoAppId:
+            app = "Video";
+            alexa.appId = videoAppId;
+            alexa.resources = nzos_strings;
+            alexa.registerHandlers(nzos_handlers);
+            context.appName = "video";
+            context.appPrompt = "video app";
         break;
     }
 
